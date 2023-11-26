c9b1abab431e12a6860bbf96a1446f892b7c5d7c
1 kytomic <kytomic1@gmail.com>
2023-11-26 21:10:51 +0800

    Implemented Cheat Mode

--- server/public/scripts/player.js
+++ server/public/scripts/player.js
@@ -3,33 +3,39 @@
 // - `x` - The initial x position of the player
 // - `y` - The initial y position of the player
 // - `gameArea` - The bounding box of the game area
-const Player = function(ctx, x, y, boxes) {
+const Player = function(ctx, x, y, boxes, playerNum, cheat_mode) {
 
     // This is the sprite sequences of the player facing different directions.
     // It contains the idling sprite sequences `idleLeft`, `idleUp`, `idleRight` and `idleDown`,
     // and the moving sprite sequences `moveLeft`, `moveUp`, `moveRight` and `moveDown`.
     const sequences = {
         /* Idling sprite sequences for facing different directions */
-        idleLeft:  { x: 0, y: 25, width: 24, height: 25, count: 3, timing: 2000, loop: false },
-        idleUp:    { x: 0, y: 50, width: 24, height: 25, count: 1, timing: 2000, loop: false },
-        idleRight: { x: 0, y: 75, width: 24, height: 25, count: 3, timing: 2000, loop: false },
-        idleDown:  { x: 0, y:  0, width: 24, height: 25, count: 3, timing: 2000, loop: false },
+        idleLeft:  { x: 0, y: 26, width: 17, height: 26, count: 1, timing: 2000, loop: false },
+        // idleUp:    { x: 0, y: 50, width: 24, height: 25, count: 1, timing: 2000, loop: false },
+        idleRight: { x: 0, y: 0, width: 17, height: 26, count: 1, timing: 2000, loop: false },
+        // idleDown:  { x: 0, y:  0, width: 24, height: 25, count: 3, timing: 2000, loop: false },
 
         /* Moving sprite sequences for facing different directions */
-        moveLeft:  { x: 0, y: 125, width: 24, height: 25, count: 10, timing: 50, loop: true },
-        moveUp:    { x: 0, y: 150, width: 24, height: 25, count: 10, timing: 50, loop: true },
-        moveRight: { x: 0, y: 175, width: 24, height: 25, count: 10, timing: 50, loop: true },
-        moveDown:  { x: 0, y: 100, width: 24, height: 25, count: 10, timing: 50, loop: true }
+        moveLeft:  { x: 0, y: 26, width: 17, height: 26, count: 2, timing: 50, loop: true },
+        // moveUp:    { x: 0, y: 150, width: 24, height: 25, count: 10, timing: 50, loop: true },
+        moveRight: { x: 0, y: 0, width: 17, height: 26, count: 2, timing: 50, loop: true },
+        // moveDown:  { x: 0, y: 100, width: 24, height: 25, count: 10, timing: 50, loop: true }
     };
 
     // This is the sprite object of the player created from the Sprite module.
     const sprite = Sprite(ctx, x, y);
 
     // The sprite object is configured for the player sprite here.
-    sprite.setSequence(sequences.idleDown)
-          .setScale(1.5)
-          .setShadowScale({ x: 0.75, y: 0.20 })
-          .useSheet("../media/player_sprite.png");
+    sprite.setScale(1.5)
+          .setShadowScale({ x: 0.75, y: 0.20 });
+
+    if (playerNum == 1) {
+        sprite.useSheet("../media/fireboy.png")
+              .setSequence(sequences.idleLeft);
+    }else{
+        sprite.useSheet("../media/icegirl.png")
+              .setSequence(sequences.idleRight);
+    }
 
     // This is the moving direction, which can be a number from 0 to 4:
     // - `0` - not moving
@@ -41,6 +47,9 @@ const Player = function(ctx, x, y, boxes) {
 
     // This is the moving speed (pixels per second) of the player
     let speed = 75;
+    if (cheat_mode) {
+        speed = 125;
+    }
 
     // This function sets the player's moving direction.
     // - `dir` - the moving direction (1: Left, 2: Up, 3: Right, 4: Down)
@@ -48,9 +57,9 @@ const Player = function(ctx, x, y, boxes) {
         if (dir >= 1 && dir <= 4 && dir != direction) {
             switch (dir) {
                 case 1: sprite.setSequence(sequences.moveLeft); break;
-                case 2: sprite.setSequence(sequences.moveUp); break;
+                // case 2: sprite.setSequence(sequences.moveUp); break;
                 case 3: sprite.setSequence(sequences.moveRight); break;
-                case 4: sprite.setSequence(sequences.moveDown); break;
+                // case 4: sprite.setSequence(sequences.moveDown); break;
             }
             direction = dir;
         }
@@ -62,22 +71,12 @@ const Player = function(ctx, x, y, boxes) {
         if (direction == dir) {
             switch (dir) {
                 case 1: sprite.setSequence(sequences.idleLeft); break;
-                case 2: sprite.setSequence(sequences.idleUp); break;
+                // case 2: sprite.setSequence(sequences.idleUp); break;
                 case 3: sprite.setSequence(sequences.idleRight); break;
-                case 4: sprite.setSequence(sequences.idleDown); break;
+                // case 4: sprite.setSequence(sequences.idleDown); break;
             }
             direction = 0;
         }
-    };
-
-    // This function speeds up the player.
-    const speedUp = function() {
-        speed = 150;
-    };
-
-    // This function slows down the player.
-    const slowDown = function() {
-        speed = 50;
     };
 
     // Jumping logic
@@ -158,8 +157,7 @@ const Player = function(ctx, x, y, boxes) {
         socket.emit('player' + playerNum.toString() + ' jump', coordinates);
     }
 
-    // This function updates the player depending on his movement.
-    // - `time` - The timestamp when this function is called
+
     const update = function(time) {
         /* Update the player if the player is moving */
         if (direction != 0) {
@@ -168,6 +166,7 @@ const Player = function(ctx, x, y, boxes) {
             /* Move the player */
             switch (direction) {
                 case 1: x -= speed / 60; break;
+                case 2: y -= speed / 60; break;
                 case 3: x += speed / 60; break;
                 case 4: y += speed / 60; break;
             }
@@ -184,7 +183,6 @@ const Player = function(ctx, x, y, boxes) {
                 sprite.setXY(x, y);
         }
 
-        /* Update the sprite object */
         sprite.update(time);
     };
 
@@ -198,12 +196,9 @@ const Player = function(ctx, x, y, boxes) {
         return collected_coin;
     }
 
-    // The methods are returned as an object here.
     return {
         move: move,
         stop: stop,
-        speedUp: speedUp,
-        slowDown: slowDown,
         getBoundingBox: sprite.getBoundingBox,
         draw: sprite.draw,
         update: update,
