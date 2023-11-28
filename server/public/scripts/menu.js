2aff20176cd544be4393f013a8b82538cb9f1b7e
wming0826 <cmwongav@connect.ust.hk>
2023-11-28 19:14:41 +0800

    menu create room

--- server/public/scripts/menu.js
+++ server/public/scripts/menu.js
@@ -13,5 +13,32 @@ const Menu = (function () {
     document.getElementById("popup-about").classList.toggle("active");
   }
 
-  return { toggleAbout };
+  function toggleCreate() {
+    document.getElementById("popup-create").classList.toggle("active");
+  }
+
+  function createRoom() {
+    var chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var roomNumber = "";
+    for (var i = 5; i > 0; --i)
+      roomNumber += chars[Math.round(Math.random() * (chars.length - 1))];
+    console.log(roomNumber);
+
+    var createBtn = document.getElementById("create-room-btn");
+    createBtn.style.display = "none";
+
+    var roomNum = document.getElementById("room-num");
+    roomNum.style.display = "block";
+
+    var roomNumText = document.getElementById("room-num-text");
+    roomNumText.innerHTML = "Room Number: " + roomNumber;
+
+    var player1 = document.getElementById("create-player1");
+    var player2 = document.getElementById("create-player2");
+
+    player1.style.display = "block";
+    player2.style.display = "block";
+  }
+
+  return { toggleAbout, toggleCreate, createRoom };
 })();
