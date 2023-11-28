c192bdd77ab77fc9c99aba100d2e51bc5a4f842e
wming0826 <cmwongav@connect.ust.hk>
2023-11-28 19:23:34 +0800

    user auth frontend

--- server/public/scripts/menu.js
+++ server/public/scripts/menu.js
@@ -9,6 +9,23 @@ const Menu = (function () {
   //         component.initialize();
   //     }
   // };
+
+  var loginForm = document.getElementById("login");
+  var registerForm = document.getElementById("register");
+  var toggleBotton = document.getElementById("btn");
+
+  function toggleRegistration() {
+    loginForm.style.left = "-400px";
+    registerForm.style.left = "50px";
+    toggleBotton.style.left = "110px";
+  }
+
+  function toggleLogin() {
+    loginForm.style.left = "50px";
+    registerForm.style.left = "450px";
+    toggleBotton.style.left = "0";
+  }
+
   function toggleAbout() {
     document.getElementById("popup-about").classList.toggle("active");
   }
@@ -44,5 +61,12 @@ const Menu = (function () {
     player2.style.display = "block";
   }
 
-  return { toggleAbout, toggleCreate, createRoom, toggleJoin };
+  return {
+    toggleAbout,
+    toggleCreate,
+    createRoom,
+    toggleJoin,
+    toggleRegistration,
+    toggleLogin,
+  };
 })();
